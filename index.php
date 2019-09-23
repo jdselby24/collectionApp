f065852f0bd905463079d4416ef5f728212a7643
jdselby24 <jdselby24@gmail.com>
2019-09-23 14:59:16 +0100

    Added HTML and CSS template

--- index.php
+++ index.php
@@ -3,3 +3,20 @@ require_once('functions.php');
 
 $db = connectDB();
 $collection = getCollection($db);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<title>Josh's Car Collection</title>
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+<head>
+
+</head>
+
+<body>
+<h1> Josh's Car Collection</h1>
+</body>
+
+</html>
