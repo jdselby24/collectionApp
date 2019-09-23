53e3f66c88bebd9a076915d96cd09986ae72a93a
jdselby24 <jdselby24@gmail.com>
2019-09-23 13:28:19 +0100

    updated DB

--- db/collection.sql
+++ db/collection.sql
@@ -6,8 +6,8 @@
 # https://github.com/sequelpro/sequelpro
 #
 # Host: 127.0.0.1 (MySQL 5.7.27)
-# Database: collectionApp
-# Generation Time: 2019-09-23 12:03:24 +0000
+# Database: joshCollectionApp
+# Generation Time: 2019-09-23 12:28:00 +0000
 # ************************************************************
 
 
@@ -20,43 +20,43 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table collection
+# Dump of table cars
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `collection`;
+DROP TABLE IF EXISTS `cars`;
 
-CREATE TABLE `collection` (
+CREATE TABLE `cars` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `type` char(255) DEFAULT NULL,
-  `manufacturer` char(255) DEFAULT NULL,
-  `model` char(255) DEFAULT NULL,
-  `year` int(4) DEFAULT NULL,
-  `regNo` char(16) DEFAULT NULL,
-  `color` char(255) DEFAULT NULL,
-  `fuel` char(128) DEFAULT NULL,
-  `engineLayout` char(4) DEFAULT NULL,
-  `engineDisplacement` int(5) DEFAULT NULL,
-  `accel` float DEFAULT NULL,
-  `power` int(16) DEFAULT NULL,
-  `torque` int(16) DEFAULT NULL,
-  `driveTrain` char(255) DEFAULT NULL,
-  `numberOfDoors` int(16) DEFAULT NULL,
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `manufacturer` varchar(255) NOT NULL DEFAULT '',
+  `model` varchar(255) NOT NULL DEFAULT '',
+  `year` year(4) NOT NULL,
+  `regNo` varchar(16) NOT NULL DEFAULT '',
+  `color` varchar(255) NOT NULL DEFAULT '',
+  `fuel` varchar(128) NOT NULL DEFAULT '',
+  `engineLayout` varchar(4) NOT NULL DEFAULT '',
+  `engineDisplacement` int(5) unsigned NOT NULL,
+  `driveTrain` enum('FWD','RWD','AWD','4WD') DEFAULT 'FWD',
+  `accel` float unsigned NOT NULL,
+  `power` int(16) unsigned NOT NULL,
+  `torque` int(16) unsigned NOT NULL,
+  `numberOfDoors` int(16) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-LOCK TABLES `collection` WRITE;
-/*!40000 ALTER TABLE `collection` DISABLE KEYS */;
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
 
-INSERT INTO `collection` (`id`, `type`, `manufacturer`, `model`, `year`, `regNo`, `color`, `fuel`, `engineLayout`, `engineDisplacement`, `accel`, `power`, `torque`, `driveTrain`, `numberOfDoors`)
+INSERT INTO `cars` (`id`, `type`, `manufacturer`, `model`, `year`, `regNo`, `color`, `fuel`, `engineLayout`, `engineDisplacement`, `driveTrain`, `accel`, `power`, `torque`, `numberOfDoors`)
 VALUES
-	(1,'Hatchback','Volkswagen','Golf Mk4 V5',1999,'V610 EDX','Blue','Petrol','VR5',2324,8.5,150,205,'FWD',5),
-	(2,'Station Wagon','Land Rover','110 V8 SW',1990,'G291 HOB','White','Petrol','V8',3528,14.4,136,253,'4WD',5),
-	(3,'Hatchback','Volkswagen','Golf Mk6 TDI BlueMotion',2012,'WG12 DWL','White','Diesel','I4',1598,10.1,105,250,'FWD',5),
-	(4,'Roadster','Mazda','MX-5 (NA) 1.8i',1994,'M420 AHW','Red','Petrol','I4',1839,8.3,128,149,'RWD',2),
-	(5,'Pickup','Nissan','Navara (D22) Outlaw 2.5dCi',2005,'VN05 GJE','Silver','Diesel','I4',2488,15.1,132,304,'4WD',4),
-	(6,'Hatchback','Ford','Focus Mk1',2004,'DE04 ZPG','Dark Blue','Petrol','I4',1596,10.9,101,145,'FWD',3);
+	(1,'Hatchback','Volkswagen','Golf Mk4 V5','1999','V610 EDX','Blue','Petrol','VR5',2324,'FWD',8.5,150,205,5),
+	(2,'Station Wagon','Land Rover','110 V8 SW','1990','G291 HOB','White','Petrol','V8',3528,'4WD',14.4,136,253,5),
+	(3,'Hatchback','Volkswagen','Golf Mk6 TDI BlueMotion','2012','WG12 DWL','White','Diesel','I4',1598,'FWD',10.1,105,250,5),
+	(4,'Roadster','Mazda','MX-5 (NA) 1.8i','1994','M420 AHW','Red','Petrol','I4',1839,'RWD',8.3,128,149,2),
+	(5,'Pickup','Nissan','Navara (D22) Outlaw 2.5dCi','2005','VN05 GJE','Silver','Diesel','I4',2488,'4WD',15.1,132,304,4),
+	(6,'Hatchback','Ford','Focus Mk1','2004','DE04 ZPG','Dark Blue','Petrol','I4',1596,'FWD',10.9,101,145,3);
 
-/*!40000 ALTER TABLE `collection` ENABLE KEYS */;
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
