74ab866990579a34fcccce88e2c300f5e36ea009
jdselby24 <jdselby24@gmail.com>
2019-09-24 13:08:34 +0100

    fixed self closing tags

--- addToCollection.php
+++ addToCollection.php
@@ -2,9 +2,9 @@
 
 <head>
     <title>Josh's Car Collection - Add</title>
-    <link rel="stylesheet" type="text/css" href="normalize.css">
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="normalize.css"/>
+    <link rel="stylesheet" type="text/css" href="styles.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 <body>
