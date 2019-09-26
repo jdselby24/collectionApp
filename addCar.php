6b9c72ab6206c8b8d42d30bfa1a19d56399a581a
jdselby24 <jdselby24@gmail.com>
2019-09-26 09:32:27 +0100

    Kicks user out of addCar.php (properly) if they haven't come from the form

--- addCar.php
+++ addCar.php
@@ -2,27 +2,31 @@
 session_start();
 require_once('functions.php');
 
-if (!empty($_POST)) {
-    header("Location: addToCollection.php");
-}
-
-$formData = $_POST;
-$db = connectDB();
-$valid = validateAddData($formData);
+if(isset($_GET['authenticated'])) {
+    if($_GET['authenticated'] === 'true') {
+        $formData = $_POST;
+        $db = connectDB();
+        $valid = validateAddData($formData);
 
 
-if ($valid["valid"] === true) {
-    $addState = addToDB($formData,$db);
-    if ($addState) {
-        header('Location: index.php');
+        if ($valid["valid"] === true) {
+            $addState = addToDB($formData,$db);
+            if ($addState) {
+                header('Location: index.php');
+            } else {
+                header('Location: addToCollection.php');
+            }
+
+        } else {
+            $formError = formError($valid);
+            $_SESSION["formError"] = $formError;
+            unset($_POST);
+            header("Location: addToCollection.php");
+        }
     } else {
-        echo "<meta http-equiv='Refresh' content='0; url=index.php' />";
+        header('Location: addToCollection.php');
     }
-
 } else {
-    $formError = formError($valid);
-    $_SESSION["formError"] = $formError;
-    unset($_POST);
-    header("Location: addToCollection.php");
+    header('Location: addToCollection.php');
 }
 ?>
